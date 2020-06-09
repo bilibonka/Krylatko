cefd49e64da677a1e660e964b02ad885809d2229
bilibonka <42473119+bilibonka@users.noreply.github.com>
2020-06-10 01:35:06 +0300

    Update laze_load.js

--- js/laze_load.js
+++ js/laze_load.js
@@ -9,20 +9,20 @@ const options = {
 function handleImg(myImg, observer) {
   myImg.forEach((myImgSingle) => {
     if (
-      myImgSingle.target.src == "http://dredok.mcdir.ru/" ||
-      myImgSingle.target.src == "http://dredok.mcdir.ru/index.html" ||
-      myImgSingle.target.src == "http://dredok.mcdir.ru/#"
+      myImgSingle.target.src == "https://bilibonka.github.io/Krylatko/" ||
+      myImgSingle.target.src == "https://bilibonka.github.io/Krylatko/index.html" ||
+      myImgSingle.target.src == "https://bilibonka.github.io/Krylatko/#"
     ) {
       if (myImgSingle.intersectionRatio) {
         loadImage(myImgSingle.target);
         switch (myImgSingle.target.src) {
-          case "http://dredok.mcdir.ru/img/header_background2.jpg":
+          case "https://bilibonka.github.io/Krylatko/img/header_background2.jpg":
             myImgSingle.target.classList.add("indentFirstSection");
             break;
-          case "http://dredok.mcdir.ru/img/sell_background2.jpg":
+          case "https://bilibonka.github.io/Krylatko/img/sell_background2.jpg":
             myImgSingle.target.classList.add("indentDiscountBlock__image");
             break;
-          case "http://dredok.mcdir.ru/img/armchairGazelle2.png":
+          case "https://bilibonka.github.io/Krylatko/img/armchairGazelle2.png":
             myImgSingle.target.classList.add(
               "indentSpecialOfferDiscount__background"
             );
